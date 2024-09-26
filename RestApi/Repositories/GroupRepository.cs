6c8bfdca8c5696a502d0f8c5e998bcf2ac2b9165
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2024-09-26 17:07:41 -0600

    metodo buscar por nombre exacto

--- RestApi/Repositories/GroupRepository.cs
+++ RestApi/Repositories/GroupRepository.cs
@@ -16,6 +16,26 @@ public class GroupRepository : IGroupRepository{
         var database = mongoClient.GetDatabase(configuration.GetValue<string>("MongoDB:Groups:DatabaseName"));
         _groups = database.GetCollection<GroupEntity>(configuration.GetValue<string>("MongoDB:Groups:CollectionName"));
     }
+
+    public async Task<GroupModel> CreateAsync(string name, Guid[] users, CancellationToken cancellationToken)
+    {
+        var group = new GroupEntity {
+            Name = name, 
+            Users = users,
+            CreatedAt = DateTime.UtcNow,
+            Id = ObjectId.GenerateNewId().ToString()
+        };
+
+        await _groups.InsertOneAsync(group, new InsertOneOptions(), cancellationToken);
+        return group.ToModel();
+    }
+
+    public async Task DeleteByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        var filter = Builders<GroupEntity>.Filter.Eq(s => s.Id, id); 
+        await _groups.DeleteOneAsync(filter, cancellationToken);
+    }
+
     public async Task<GroupModel> GetByIdAsync(string id, CancellationToken cancellationToken){
         try{
             var filter = Builders<GroupEntity>.Filter.Eq(x => x.Id, id);
@@ -26,6 +46,19 @@ public class GroupRepository : IGroupRepository{
 
             return null;
         }
+    }
+
+    public async Task<GroupModel> GetByExactNameAsync(string name, CancellationToken cancellationToken)
+    {
+        try {
+            var filter = Builders<GroupEntity>.Filter.Eq(group => group.Name, name);
+            var group = await _groups.Find(filter).FirstOrDefaultAsync(cancellationToken);
+            return group.ToModel(); 
+
+        } catch (FormatException){
+
+            return null; 
+         }
     }
     
     public async Task<IList<GroupModel>> GetGroupsByNameAsync(string name, int pageNumber, int pageSize, string orderBy, CancellationToken cancellationToken){
@@ -38,6 +71,11 @@ public class GroupRepository : IGroupRepository{
         var groups = await _groups.Find(filter).Skip(pageSize * (pageNumber - 1)).Limit(pageSize).Sort(sort).ToListAsync(cancellationToken);
         return groups.Select(group => group.ToModel()).ToList();
     }
+
+    
+
+
+
     
 
 }
